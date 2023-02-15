c9e0558d197089f247a9eb54beb9e89df011ca33
Sayed <elabadysayed@google.com>
2023-02-15 17:13:30 +0000

    Add basic before unload event

--- application.js
+++ application.js
@@ -13,13 +13,15 @@
     document.getElementById("surprisePic").src = "images/icons/gon.png";
   
   }
-  
+  function beforeunload() {
+    return "string";
+  };
+
   const beforeUnloadListener = (event) => {
-    return event.returnValue = '';
+    return "string";
   };
   
   const nameInput = document.querySelector("#name");
-  
   nameInput.addEventListener("input", (event) => {
     if (event.target.value !== "") {
       addEventListener("beforeunload", beforeUnloadListener, {capture: true});
