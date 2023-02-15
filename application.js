04f8ea6a37293173c4ba3da5132fe8a14ad14c30
Sayed <elabadysayed@google.com>
2023-02-15 16:01:52 +0000

    Add beforeunload event

--- application.js
+++ application.js
@@ -14,3 +14,16 @@
   
   }
   
+  const beforeUnloadListener = (event) => {
+    return event.returnValue = '';
+  };
+  
+  const nameInput = document.querySelector("#name");
+  
+  nameInput.addEventListener("input", (event) => {
+    if (event.target.value !== "") {
+      addEventListener("beforeunload", beforeUnloadListener, {capture: true});
+    } else {
+      removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
+    }
+  });
