7d7ad295f6a064f2dbb1edc7a6d8d1b58641f453
Sayed <elabadysayed@google.com>
2023-03-31 16:15:28 +0100

    Add verification for handle_all_urls

--- application.js
+++ application.js
@@ -1,22 +1,22 @@
-(function() {
-    'use strict';
-  
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('./service-worker.js');
-    }
-    
-})();
-  function postMessage(){
-    window.postMessage("Hey", "*")
-  };
+(function () {
+  'use strict';
 
-    var response = "";
-  function appendOutput(msg) {
-    response+=msg;
-    alert(response);
-  };
-  function testFunction(){
-    document.getElementById("surprisePic").src = "images/icons/gon.png";
-  };
-    
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.js');
+  }
+
+})();
+function postMessage() {
+  window.postMessage("Hey", "*")
+};
+
+var response = "";
+function appendOutput(msg) {
+  response += msg;
+  alert(response);
+};
+function testFunction() {
+  document.getElementById("surprisePic").src = "images/icons/gon.png";
+};
+
--- postmessage.js
+++ postmessage.js
@@ -1,23 +1,23 @@
-(function() {
+(function () {
     'use strict';
-  
+
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('./service-worker.js');
+        navigator.serviceWorker
+            .register('./service-worker.js');
     }
-    
+
 })();
 
 
-  const output = document.getElementById('history');
+const output = document.getElementById('history');
 
-  function appendOutput(msg) {
+function appendOutput(msg) {
     var log = output.textContent;
     output.textContent = log + msg + "\n"
-  };
+};
 
 
-  window.addEventListener("message", function(event) {
+window.addEventListener("message", function (event) {
     console.log("[PostMessage] Got initial message.");
     appendOutput("Got initial message.");
     const clientButton = document.getElementById('butMessageClient');
@@ -30,16 +30,16 @@
 
     port.postMessage("Connected");
 
-    
-    clientButton.addEventListener('click', function() {
-      port.postMessage(img.alt);
-      appendOutput("test")
+
+    clientButton.addEventListener('click', function () {
+        port.postMessage(img.alt);
+        appendOutput("test")
     });
 
-    port.onmessage = function(event) {
-      console.log("[PostMessage] Got Message: " + event.data);
-      appendOutput(event.data);
-      port.postMessage("ACK " + event.data);
+    port.onmessage = function (event) {
+        console.log("[PostMessage] Got Message: " + event.data);
+        appendOutput(event.data);
+        port.postMessage("ACK " + event.data);
     };
-  });
+});
 
