a89eefc7a0202e0b5091e283e5063696503f6d8b
Sayed <elabadysayed@google.com>
2023-03-31 16:09:06 +0100

    Add postMessage page

--- application.js
+++ application.js
@@ -9,7 +9,6 @@
 })();
   function postMessage(){
     window.postMessage("Hey", "*")
-    document.getElementById("f").contentWindow.postMessage("Hey", "*")
   };
 
     var response = "";
@@ -21,30 +20,3 @@
     document.getElementById("surprisePic").src = "images/icons/gon.png";
   };
     
-
-  window.addEventListener("message", function(event) {
-    console.log("[PostMessage] Got initial message.");
-    appendOutput("Got initial message.");
-    const clientButton = document.getElementById('butMessageClient');
-
-    var port = event.ports[0];
-    if (typeof port === 'undefined') return;
-
-    console.log("[PostMessage] Got message port.");
-    appendOutput("Got message port.");
-
-    port.postMessage("Connected");
-
-    
-    clientButton.addEventListener('click', function() {
-      port.postMessage(img.alt);
-      appendOutput("test")
-    });
-
-    port.onmessage = function(event) {
-      console.log("[PostMessage] Got Message: " + event.data);
-      appendOutput(event.data);
-      port.postMessage("ACK " + event.data);
-    };
-  });
-
---
+++ postmessage.js
@@ -0,0 +1,45 @@
+(function() {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('./service-worker.js');
+    }
+    
+})();
+
+
+  const output = document.getElementById('history');
+
+  function appendOutput(msg) {
+    var log = output.textContent;
+    output.textContent = log + msg + "\n"
+  };
+
+
+  window.addEventListener("message", function(event) {
+    console.log("[PostMessage] Got initial message.");
+    appendOutput("Got initial message.");
+    const clientButton = document.getElementById('butMessageClient');
+
+    var port = event.ports[0];
+    if (typeof port === 'undefined') return;
+
+    console.log("[PostMessage] Got message port.");
+    appendOutput("Got message port.");
+
+    port.postMessage("Connected");
+
+    
+    clientButton.addEventListener('click', function() {
+      port.postMessage(img.alt);
+      appendOutput("test")
+    });
+
+    port.onmessage = function(event) {
+      console.log("[PostMessage] Got Message: " + event.data);
+      appendOutput(event.data);
+      port.postMessage("ACK " + event.data);
+    };
+  });
+
