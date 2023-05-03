0fddf7b69a69f5d2bedbed2f9fa169c14011004b
Sayed <elabadysayed@google.com>
2023-05-03 18:12:06 +0100

    Add origin to log message

--- postmessage.js
+++ postmessage.js
@@ -24,7 +24,7 @@ window.addEventListener("message", function (event) {
 
   iframe.contentWindow.postMessage("this should be delivered to an iframe", "*", [port]);
   port.onmessage = function(event) {
-    console.log("[PostMessage] Got Message: " + event.data);
+    console.log("[PostMessage] Got Message: " + event.data + " " + event.origin);
     appendOutput(event.data);
   };
 });
